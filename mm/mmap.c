c0705dd7485dc84ede7b44130590cfe6184f4ecc
UpInTheAir <upintheair.xda@gmail.com>
2016-01-18 10:18:39 +0700

    mm/mmap.c: fix compile from commit 02411122e5907f00e180cb95debd738f6a5442e2
    Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -2801,7 +2801,8 @@ void exit_mmap(struct mm_struct *mm)
 
 	mm->mmap = NULL;
 	mm->mm_rb = RB_ROOT;
-	mm->mmap_cache = NULL;
+	/* Kill the cache */
+	vmacache_invalidate(mm);
 	up_write(&mm->mmap_sem);
 
 	WARN_ON(mm->nr_ptes > (FIRST_USER_ADDRESS+PMD_SIZE-1)>>PMD_SHIFT);
