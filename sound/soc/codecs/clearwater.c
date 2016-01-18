94d48295f1b81d3baea0425b3fdd64d5e0e276cc
UpInTheAir <upintheair.xda@gmail.com>
2016-01-18 20:06:31 +0700

    sound-control: make Clearwater audio controls compile optional
    Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

--- sound/soc/codecs/clearwater.c
+++ sound/soc/codecs/clearwater.c
@@ -28,7 +28,9 @@
 
 #include <linux/mfd/arizona/core.h>
 #include <linux/mfd/arizona/registers.h>
+#ifdef CONFIG_SND_SOC_ARIZONA_CONTROL
 #include <linux/mfd/arizona/control.h>
+#endif
 
 #include "arizona.h"
 #include "wm_adsp.h"
@@ -2961,8 +2963,10 @@ static int clearwater_codec_probe(struct snd_soc_codec *codec)
 			ret);
 		return ret;
 	}
-	
+
+#ifdef CONFIG_SND_SOC_ARIZONA_CONTROL
 	arizona_control_init(codec);
+#endif
 
 	return 0;
 }
