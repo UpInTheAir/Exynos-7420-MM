07ab6391de7c562e47b366ea30077c4a8fd28427
lyapota <lyapic@ukr.net>
2016-07-26 08:05:10 +0700

    DEVFEQ: smart reset freq limit
    Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

--- drivers/cpufreq/exynos-mp-cpufreq.c
+++ drivers/cpufreq/exynos-mp-cpufreq.c
@@ -43,6 +43,10 @@
 #include <asm/smp_plat.h>
 #include <asm/cputype.h>
 
+#ifdef CONFIG_CPU_THERMAL_IPA
+#include "cpu_load_metric.h"
+#endif
+
 #include <mach/cpufreq.h>
 #include <mach/asv-exynos.h>
 #include <mach/apm-exynos.h>
@@ -122,6 +126,8 @@ static bool hmp_boosted = false;
 static bool cluster1_hotplugged = false;
 #endif
 
+static bool in_worque = false;
+
 /* Include CPU mask of each cluster */
 cluster_type exynos_boot_cluster;
 static cluster_type boot_cluster;
@@ -156,9 +162,13 @@ static int qos_min_class[CL_END] = {PM_QOS_CLUSTER0_FREQ_MIN, PM_QOS_CLUSTER1_FR
 //static int qos_max_default_value[CL_END] = {PM_QOS_CLUSTER0_FREQ_MAX_DEFAULT_VALUE, PM_QOS_CLUSTER1_FREQ_MAX_DEFAULT_VALUE};
 static int qos_min_default_value[CL_END] = {PM_QOS_CLUSTER0_FREQ_MIN_DEFAULT_VALUE, PM_QOS_CLUSTER1_FREQ_MIN_DEFAULT_VALUE};
 
-// reset DVFS
+// Reset DVFS
 #ifdef CONFIG_PM
+#ifdef CONFIG_CPU_THERMAL_IPA
+#define DVFS_RESET_SEC 5
+#else
 #define DVFS_RESET_SEC 15
+#endif
 static struct delayed_work dvfs_reset_work;
 static struct workqueue_struct *dvfs_reset_wq;
 #endif
@@ -1319,9 +1329,12 @@ static ssize_t store_cpufreq_min_limit(struct kobject *kobj, struct attribute *a
 
 	save_cpufreq_min_limit(cluster1_input);
 	cancel_delayed_work_sync(&dvfs_reset_work);
-	if (cluster1_input > 0)
+	in_worque = false;
+	if (cluster1_input > 0) {
 		queue_delayed_work_on(0, dvfs_reset_wq, &dvfs_reset_work,
 			DVFS_RESET_SEC * HZ);
+		in_worque = true;
+	}
 
 	return count;
 }
@@ -1412,22 +1425,41 @@ static ssize_t store_cpufreq_max_limit(struct kobject *kobj, struct attribute *a
 
 	save_cpufreq_max_limit(cluster1_input);
 	cancel_delayed_work_sync(&dvfs_reset_work);
-	if (cluster1_input > 0)
+	in_worque = false;
+	if (cluster1_input > 0) {
 		queue_delayed_work_on(0, dvfs_reset_wq, &dvfs_reset_work,
 			DVFS_RESET_SEC * HZ);
+		in_worque = true;
+	}
 
 	return count;
 }
 
 static void dvfs_reset_work_fn(struct work_struct *work)
 {
-	pr_info("%s++: DVFS timed out(%d)! Resetting with -1\n", __func__, DVFS_RESET_SEC);
+#ifdef CONFIG_CPU_THERMAL_IPA
+	int load, freq;
 
-	save_cpufreq_min_limit(-1);
-	msleep(20);
-	save_cpufreq_max_limit(-1);
+	cpu_load_metric_get(&load, &freq);
 
-	pr_info("%s--\n", __func__);
+	if (load <= 25) {
+#endif
+		pr_info("%s++: BOOST timed out(%d)! Resetting with -1\n", __func__, DVFS_RESET_SEC);
+
+		save_cpufreq_min_limit(-1);
+		msleep(20);
+		save_cpufreq_max_limit(-1);
+
+		pr_info("%s--\n", __func__);
+
+		in_worque = false;
+#ifdef CONFIG_CPU_THERMAL_IPA
+	} else {
+		if (in_worque)
+			queue_delayed_work_on(0, dvfs_reset_wq, &dvfs_reset_work,
+				DVFS_RESET_SEC * HZ);
+	}
+#endif
 }
 #endif
 
